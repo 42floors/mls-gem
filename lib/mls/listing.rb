4219e3fb29a7f94b8ffc9924b0aaf5af0ec7056f
Jon Bracy <jonbracy@gmail.com>
2012-05-03 17:05:54 -0700

    Changed way listing is named again, got overwritten

--- lib/mls/listing.rb
+++ lib/mls/listing.rb
@@ -74,18 +74,21 @@ class MLS::Listing < MLS::Resource
   end
 
   def name
-    return property.name if property.name && !property.name.empty?
+    return attributes[:name] if attributes[:name] && !attributes[:name].empty?
   
-    name = property.name
-    if kind == 'lease' && space_type == 'unit'
-      name += ", Unit #{unit}" if unit && !unit.empty?
-    elsif kind == 'lease' && space_type == 'floor'
-      name += ", Floor #{floor}" if floor && !floor.empty?
+    if property.name
+      name = property.name
+      if kind == 'lease' && space_type == 'unit'
+        name += ", Unit #{unit}" if unit && !unit.empty?
+      elsif kind == 'lease' && space_type == 'floor'
+        name += ", Floor #{floor}" if floor && !floor.empty?
+      end
+      return name
     end
     
-    name
+    return address.formatted_address.sub(/,.+$/,'')
   end
-
+  
   def available_on
     Date.parse(attributes[:available_on])
   end
