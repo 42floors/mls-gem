048471cef98a890973d36c67b00cd6fb6cf5c095
James R. Bracy <waratuman@gmail.com>
2012-05-10 12:05:36 -0700

    Adding address support.

--- lib/mls/address.rb
+++ lib/mls/address.rb
@@ -3,5 +3,10 @@ class MLS::Address < MLS::Resource
   def self.query(q)
     get :query, :query => q
   end
-  
+
+  private
+
+  def self.instantiate_collection(collection, prefix_options = {})
+    collection['addresses'].collect! { |record| instantiate_record(record, prefix_options) }
+  end  
 end
