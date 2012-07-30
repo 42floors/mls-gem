af858f2a51e7367af3f1e08b8cee755ebd8700b1
Jon Bracy <jonbracy@gmail.com>
2012-07-30 13:12:41 -0700

    supporting more converstion to/from datetime

--- lib/mls/properties/datetime.rb
+++ lib/mls/properties/datetime.rb
@@ -5,13 +5,15 @@ class MLS::Property::DateTime < MLS::Property
       ::DateTime.iso8601(value)
     elsif value.nil? || value.is_a?(::DateTime)
       value
+    elsif value.is_a?(::Time) || value.is_a?(::Date)
+      value.to_datetime
     else
       raise 'unsupported date type'
     end
   end
   
   def dump(value)
-    if value.is_a?(::DateTime)
+    if value.is_a?(::DateTime) || value.is_a?(::Time) || value.is_a?(::Date)
       value.iso8601
     elsif value.nil?
       nil
