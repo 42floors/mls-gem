4d4bc8af81cc3303036009b50a6002f7bb84d484
Jon Bracy <jonbracy@gmail.com>
2012-09-18 12:13:29 -0700

    removed slug_deprecated

--- lib/mls/models/address.rb
+++ lib/mls/models/address.rb
@@ -3,7 +3,6 @@ class MLS::Address < MLS::Resource
   property :id, Fixnum,   :serialize => :if_present
   property :name, String, :serialize => :false
   property :slug, String,   :serialize => :if_present
-  property :slug_deprecated, String,   :serialize => :if_present
   
   property :latitude, Decimal
   property :longitude, Decimal
