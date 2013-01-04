c2ed0ad6fe36ecbe6ac490fe2702d1a695fc2dde
Dana Levine <dana11235@gmail.com>
2013-01-04 14:27:12 -0800

    Video thumbnail support.

--- lib/mls/models/video.rb
+++ lib/mls/models/video.rb
@@ -2,11 +2,17 @@ class MLS::Video < MLS::Resource
   property :id, Fixnum
   property :vts_key, String
   property :video_type, String
-  property :subject_type, String
+  property :subject_type, String, :serialize => false
   property :created_at, DateTime, :serialize => :if_present
   property :updated_at, DateTime, :serialize => :if_present
+
+  property :photo_id, Fixnum, :serialize => :if_present
+
+  attr_accessor :photo
 end
 
 class MLS::Video::Parser < MLS::Parser
-
+  def photo=(photo)
+    @object.photo = MLS::Photo::Parser.build(photo)
+  end
 end
