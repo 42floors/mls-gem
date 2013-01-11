373bbc685545fb7cf8ea956bd364af7945d12706
Dana Levine <dana11235@gmail.com>
2013-01-10 21:59:36 -0800

    mls updates for accepting/declining tour requests.

--- lib/mls/models/tour_request.rb
+++ lib/mls/models/tour_request.rb
@@ -8,6 +8,12 @@ class MLS::TourRequest < MLS::Resource
   property :company,                      String
   property :population,                   String
   property :growing,                      Boolean
+  property :updated_by_id,                Fixnum
+  property :status,                       String
+  property :reasons_to_decline,           String,    :serialize => :if_present
+
+  property :random_token,                 String,    :serialize => :false
+
   property :created_at,                   DateTime,  :serialize => :false
   property :updated_at,                   DateTime,  :serialize => :false
 
@@ -15,12 +21,23 @@ class MLS::TourRequest < MLS::Resource
 
   def claim(agent)
     MLS.post("/tour_requests/#{id}/claim", {:agent_id => agent.id}) do |response, code|
-      if code == 200
-        true
-      else
-        false
-      end
+      return code == 200
     end
+  end
+
+  def decline(agent, reasons=nil)
+    MLS.post("/tour_requests/#{id}/decline", 
+      {:agent_id => agent.id, :reasons_to_decline => reasons}) do |response, code|
+      return code == 200
+    end
+  end
+
+  def claimed?
+    status == "CLAIMED"
+  end
+
+  def declined?
+    status == "DECLINED"
   end
 
   class << self
