6df80685555c3f2faf5533da0eeb7ea6e6cbeb19
Darren Nix <me@darrennix.com>
2013-05-22 20:24:58 -0700

    Replace system_phone with twilio_extension on account

--- lib/mls/models/account.rb
+++ lib/mls/models/account.rb
@@ -12,7 +12,7 @@ class MLS::Account < MLS::Resource
   property :ghost,                   Boolean, :serialize => false, :default => false
   
   property :phone,                   String
-  property :system_phone,            String,  :serialize => false
+  property :twilio_extension,        String,  :serialize => false
   property :company,                 String
   property :license,                 String
   property :linkedin,                String
