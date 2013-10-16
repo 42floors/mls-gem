4a6047cf39f68eddd04789e371911e2941867f23
Julian Simioni <julian.simioni@gmail.com>
2013-10-16 11:45:13 -0700

    Whitespace

--- lib/mls/parser.rb
+++ lib/mls/parser.rb
@@ -1,26 +1,26 @@
 class MLS::Parser
-  
+
   attr_reader :object
-  
+
   def initialize(object=nil, persisted=true)
     @object = object || self.class.object_class.new({}, persisted)
   end
-  
+
   def parse(data)
     attributes = extract_attributes(data)[self.class.root_element]
     update_attributes(attributes)
     object
   end
-  
+
   def build(attributes)
     update_attributes(attributes)
     object
   end
-  
+
   def update_attributes(attributes)
     attributes.each {|k,v| self.send("#{k}=".to_sym, v) } if attributes
   end
-  
+
   def self.parse(data)
     self.new.parse(data)
   end
@@ -34,27 +34,27 @@ class MLS::Parser
     end
     collection
   end
-  
+
   def self.parse_collection(data, options={})
     build_collection(extract_attributes(data), options)
   end
-  
+
   def self.build(attributes)
     self.new.build(attributes)
   end
-  
+
   def self.update(object, data)
-    self.new(object).parse(data)    
+    self.new(object).parse(data)
   end
-  
+
   def method_missing(method, *args, &block)
     object.__send__(method, *args, &block) if object.methods.include?(method)
-  end  
-  
+  end
+
   def extract_attributes(data)
     Yajl::Parser.new(:symbolize_keys => true).parse(data)
   end
-  
+
   def self.extract_attributes(data)
     Yajl::Parser.new(:symbolize_keys => true).parse(data)
   end
@@ -62,13 +62,13 @@ class MLS::Parser
   def self.object_class
     @object_class ||= ActiveSupport::Inflector.constantize(self.to_s.sub('::Parser',''))
   end
-  
+
   def self.root_element
     object_class.root_element
   end
-  
+
   def self.collection_root_element
     object_class.collection_root_element
   end
-  
+
 end
--- test/units/models/test_listing.rb
+++ test/units/models/test_listing.rb
@@ -4,7 +4,7 @@ class TestListing < ::Test::Unit::TestCase
 
   def test_properties
     listing = MLS::Listing.new
-  
+
     assert listing.respond_to?(:id)
     assert listing.respond_to?(:address_id)
     assert listing.respond_to?(:use)
@@ -39,96 +39,96 @@ class TestListing < ::Test::Unit::TestCase
     assert listing.respond_to?(:created_at)
     assert listing.respond_to?(:updated_at)
   end
-  
+
   def test_attr_accessors
     listing = MLS::Listing.new
-  
+
     assert listing.respond_to?(:address)
     assert listing.respond_to?(:agents)
   end
-  
+
   def test_instance_methods
     listing = MLS::Listing.new
-  
+
     assert listing.respond_to?(:photos)
   end
-  
+
   def test_class_methods
     assert MLS::Listing.respond_to?(:find)
   end
-  
+
   test '#request_tour for email without an account' do
     @listing = FactoryGirl.create(:listing)
     @name = Faker::Name.name
     @email = Faker::Internet.email
     tr = @listing.request_tour(@name, @email)
-  
-  
+
+
     assert_equal({}, tr.errors)
     assert_equal({}, tr.account.errors)
     # TODO assert_equal({}, tr.listing.errors)
     assert tr.id
   end
-  
+
   test '#request_tour for email on a ghost account' do
     @account = FactoryGirl.create(:ghost_account)
     @listing = FactoryGirl.create(:listing)
-    
+
     tr = @listing.request_tour(@account.name, @account.email)
     assert_equal({}, tr.errors)
     assert_equal({}, tr.account.errors)
     # TODO assert_equal({}, tr.listing.errors)
     assert tr.id
   end
-  
+
   test '#request_tour for email on an account' do
     @account = FactoryGirl.create(:account)
     @listing = FactoryGirl.create(:listing)
-  
+
     tr = @listing.request_tour(@account.name, @account.email)
     assert_equal({}, tr.errors)
     assert_equal({}, tr.account.errors)
     # TODO assert_equal({}, tr.listing.errors)
     assert tr.id
   end
-  
+
   test '#request_tour for an non-existant listing' do
     @listing = FactoryGirl.build(:listing, :id => 94332)
-    
+
     assert_raises(MLS::Exception::NotFound) do
       @listing.request_tour(Faker::Name.name, Faker::Internet.email)
     end
   end
-  
+
   test '#request_tour without and account name' do
     @listing = FactoryGirl.create(:listing)
-    
+
     tr = @listing.request_tour('', Faker::Internet.email)
     assert !tr.id
     assert_equal({:name => ["can't be blank"]}, tr.account.errors)
-    
+
     tr = @listing.request_tour(nil, Faker::Internet.email)
     assert !tr.id
     assert_equal({:name => ["can't be blank"]}, tr.account.errors)
   end
-  
+
   test '#request_tour without an account email' do
     @listing = FactoryGirl.create(:listing)
-    
+
     tr = @listing.request_tour(Faker::Name.name, '')
     assert !tr.id
     assert_equal({:email => ["can't be blank", "is invalid"]}, tr.account.errors)
-    
+
     tr = @listing.request_tour(Faker::Name.name, nil)
     assert !tr.id
     # assert !tr.persisted? #TODO move to persisted being based of id?
     assert_equal({:email => ["can't be blank", "is invalid"]}, tr.account.errors)
   end
-  
+
   test '#request_tour with an account email' do
     @account = FactoryGirl.create(:account)
     @listing = FactoryGirl.create(:listing)
-    
+
     tr = @listing.request_tour('', @account.email) # TODO should this try to set the name of the account?
     assert_equal({}, tr.errors)
     assert_equal({}, tr.account.errors)
@@ -136,38 +136,38 @@ class TestListing < ::Test::Unit::TestCase
     assert tr.id
     assert tr.persisted?
   end
-  
+
   test '#request_tour multiple times for a listing' do
     @account = FactoryGirl.create(:account)
     @listing = FactoryGirl.create(:listing)
-    
+
     tr1 = @listing.request_tour(@account.name, @account.email)
     assert_equal({}, tr1.errors) # TODO should errors be here for account?
     assert_equal({}, tr1.account.errors)
     # TODO assert_equal({}, tr.listing.errors)
     assert tr1.persisted?
-      
+
     tr2 = @listing.request_tour(@account.name, @account.email)
     assert_equal({}, tr2.errors)
     assert_equal({}, tr2.account.errors)
     # TODO assert_equal({}, tr.listing.errors)
     assert tr2.persisted?
-    
+
     assert_not_equal tr1.id, tr2.id
   end
-  
+
   test '#request_tour with optional info' do
     @listing = FactoryGirl.create(:listing)
-    
+
     info = {:company => '42Floors', :population => 10, :funding => 'string thing', :move_in_date => '2012-09-12'}
     tr = @listing.request_tour(Faker::Name.name, Faker::Internet.email, info)
-  
+
     assert tr.id
     assert_equal '42Floors', info[:company]
     assert_equal 10, info[:population]
     assert_equal 'string thing', info[:funding]
     assert_equal '2012-09-12', info[:move_in_date]
-    
+
     tr = @listing.request_tour('', nil, info)
     assert !tr.id
     assert_equal '42Floors', info[:company]
@@ -175,10 +175,10 @@ class TestListing < ::Test::Unit::TestCase
     assert_equal 'string thing', info[:funding]
     assert_equal '2012-09-12', info[:move_in_date]
   end
-  
+
   def test_rate_per_sqft_per_month
     listing = MLS::Listing.new(:rate => 10.5, :rate_units => '/sqft/mo', :size => 5)
-  
+
     assert_equal 10.5,  listing.rate
     assert_equal 10.5,  listing.rate('/sqft/mo')
     assert_equal 126,   listing.rate('/sqft/yr')
@@ -189,10 +189,10 @@ class TestListing < ::Test::Unit::TestCase
       listing.rate('/random')
     end
   end
-  
+
   def test_rate_per_sqft_per_year
     listing = MLS::Listing.new(:rate => 126, :rate_units => '/sqft/yr', :size => 5)
-  
+
     assert_equal 126,  listing.rate
     assert_equal 10.5,  listing.rate('/sqft/mo')
     assert_equal 126,   listing.rate('/sqft/yr')
@@ -203,10 +203,10 @@ class TestListing < ::Test::Unit::TestCase
       listing.rate('/random')
     end
   end
-  
+
   def test_rate_per_month
     listing = MLS::Listing.new(:rate => 52.5, :rate_units => '/mo', :size => 5)
-  
+
     assert_equal 52.5,  listing.rate
     assert_equal 10.5,  listing.rate('/sqft/mo')
     assert_equal 126,   listing.rate('/sqft/yr')
@@ -217,10 +217,10 @@ class TestListing < ::Test::Unit::TestCase
       listing.rate('/random')
     end
   end
-  
+
   def test_rate_per_year
     listing = MLS::Listing.new(:rate => 630, :rate_units => '/yr', :size => 5)
-  
+
     assert_equal 630,  listing.rate
     assert_equal 10.5,  listing.rate('/sqft/mo')
     assert_equal 126,   listing.rate('/sqft/yr')
@@ -231,10 +231,10 @@ class TestListing < ::Test::Unit::TestCase
       listing.rate('/random')
     end
   end
-  
+
   def test_rate_per_desk_per_month
     listing = MLS::Listing.new(:rate => 2100, :rate_units => '/desk/mo', :size => 5)
-  
+
     assert_equal 2100,  listing.rate
     assert_equal 10.5,  listing.rate('/sqft/mo')
     assert_equal 126,   listing.rate('/sqft/yr')
@@ -245,7 +245,7 @@ class TestListing < ::Test::Unit::TestCase
       listing.rate('/random')
     end
   end
-  
+
   def test_rate_percision
     listing = MLS::Listing.new(:rate => 47, :rate_units => '/sqft/yr', :size => 5)
 
@@ -255,10 +255,10 @@ class TestListing < ::Test::Unit::TestCase
     assert_equal 235, listing.rate('/yr')
     assert_equal 783.33,   listing.rate('/desk/mo')
   end
-  
+
   def test_null_rate
     listing = MLS::Listing.new(:rate => nil, :rate_units => '/yr', :size => 5)
-  
+
     assert_equal nil,  listing.rate
     assert_equal nil,  listing.rate('/sqft/mo')
     assert_equal nil,   listing.rate('/sqft/yr')
