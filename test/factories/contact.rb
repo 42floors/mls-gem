db160155af3e705c9b321ec66b309846b93782bc
Julian Simioni <julian.simioni@gmail.com>
2013-10-17 15:20:10 -0700

    Remove test/factories dir, factories are in lib/mls/factories

--- test/factories/contact.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :tour, :class => MLS::Tour do
-    message { Faker::Lorem.paragraph }
-    company { Faker::Company.email }
-    population { Kernel.rand(2..200) }
-    funding { Faker::Lorem.sentance }
-    move_in_date '1234-12-21'
-  end  
-end
