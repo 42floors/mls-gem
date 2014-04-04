c4bb2780ac052a08fe22c6e79cf54cb442085778
Aaron <aaron.oconnell@gmail.com>
2014-04-04 10:45:52 -0700

    allow original ip to be forwarded to the api

--- lib/mls.rb
+++ lib/mls.rb
@@ -25,7 +25,7 @@ class MLS
 
   attr_reader :url, :user_agent
   attr_writer :asset_host, :image_host, :agent_profile
-  attr_accessor :api_key, :cookie_jar, :logger, :branch, :server
+  attr_accessor :api_key, :cookie_jar, :logger, :branch, :server, :forwarded_for
 
   # Sets the API Token and Host of the MLS Server
   #
@@ -85,6 +85,7 @@ class MLS
     }
     headers['X-42Floors-Branch'] = branch if branch
     headers['X-42Floors-Server'] = server if server
+    headers['X-Forwarded-For'] = forwarded_for if forwarded_for
     headers
   end
 
