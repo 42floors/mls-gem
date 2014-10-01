6d8ebe29a28ce72e1547b5831dfb69e9221850c4
Jon Bracy <jonbracy@gmail.com>
2014-10-01 11:37:54 -0700

    Updated Session::authenticate method

--- lib/mls/session.rb
+++ lib/mls/session.rb
@@ -2,8 +2,17 @@ class Session < MLS::Model
 
   belongs_to :account
 
-  def self.authenticate(email, password)
+  # Authenticate with email and password.
+  # Returns either the newly created session or nil
+  def self.authenticate(email, password=nil)
+    if email.is_a? Hash
+      password = email[:password]
+      email = email[:email]
+    end
+    
     Session.create(:email => email, :password => password)
+  rescue Sunstone::Exception::Unauthorized
+    nil
   end
 
   def self.authenticate_by_token(token)
