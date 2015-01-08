cc518ae15e66a913c3d4a565caecf6bde805baf0
Jono O'Shea <jono.oshea@gmail.com>
2015-01-08 15:55:55 -0800

    Added floorplan to unit

--- lib/mls/unit.rb
+++ lib/mls/unit.rb
@@ -3,10 +3,11 @@ class Unit < MLS::Model
   include MLS::Avatar
 
   belongs_to :property
+  belongs_to :floorplan
 
   has_many :listings
   has_many :photos, -> { order('photos.order ASC') }, :as => :subject, :inverse_of => :subject
-  
+
   def tags
     read_attribute(:tags) || []
   end
