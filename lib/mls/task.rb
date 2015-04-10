7bd1043d4ee38cebc7af9ffeb0847eae556a9c85
Ben Ehmke <benehmke@gmail.com>
2015-04-10 18:01:54 -0500

    remove sti for task

--- lib/mls/task.rb
+++ lib/mls/task.rb
@@ -1,10 +1,12 @@
 class Task < MLS::Model
+  self.inheritance_column = nil
 
   belongs_to  :subject, :polymorphic => true
   belongs_to  :account
   
   has_many :events
   has_many :mistakes
+  has_many :reviews, :class_name => "Task", :as => :subject, :inverse_of => :subject
   
   def for_source?
     subject_type == "Source"
@@ -12,6 +14,18 @@ class Task < MLS::Model
   
   def for_task?
     subject_type == "Taks"
+  end
+  
+  def review?
+    type == "review"
+  end
+  
+  def fix?
+    type == "fix"
+  end
+  
+  def perform?
+    type == "perform"
   end
   
 end
