4d37ede31e6622ee1824b7a812a926d9a0d4502e
Jon Bracy <jonbracy@gmail.com>
2015-04-13 11:39:09 -0700

    Added Region.cover_photo_url helper

--- lib/mls/region.rb
+++ lib/mls/region.rb
@@ -9,5 +9,28 @@ class Region < MLS::Model
   has_and_belongs_to_many :parents, :join_table => 'regions_regions', :class_name => 'Region', :foreign_key => 'child_id', :association_foreign_key => 'parent_id'
   has_and_belongs_to_many :children, :join_table => 'regions_regions', :class_name => 'Region', :foreign_key => 'parent_id', :association_foreign_key => 'child_id'
   
-  
+  def cover_photo_url(options={})
+
+    options.reverse_merge!({
+      :style => nil,
+      :bg => nil,
+      :protocol => 'https',
+      :format => "jpg",
+      :host => MLS.image_host
+    });
+
+    url_params = { s: options[:style], bg: options[:bg] }.select{ |k, v| v }
+
+    if options[:protocol] == :relative # Protocol Relative
+      result = '//'
+    else options[:protocol]
+      result = "#{options[:protocol]}://"
+    end
+
+    result += "#{options[:host]}/#{cover_photo_digest}.#{options[:format]}"
+    result += "?#{url_params.to_param}" if url_params.size > 0
+
+    result
+  end
+
 end
