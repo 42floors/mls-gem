610836c03dc83b654be55d3c2d42700491f04b0e
James R. Bracy <waratuman@gmail.com>
2015-05-21 11:47:08 -0700

    Doc updates

--- lib/mls/document.rb
+++ lib/mls/document.rb
@@ -43,6 +43,30 @@ class Document < MLS::Model
 end
 
 class Image < Document
+
+  def url(options={})
+    options.reverse_merge!({
+      :style => nil,
+      :bg => nil,
+      :protocol => 'https',
+      :format => "jpg",
+      :host => MLS.image_host
+    });
+
+    url_params = {s: options[:style], bg: options[:bg]}.select{ |k, v| v }
+
+    if options[:protocol] == :relative # Protocol Relative
+      result = '//'
+    else options[:protocol]
+      result = "#{options[:protocol]}://"
+    end
+
+    result += "#{options[:host]}/#{hash_key}.#{options[:format]}"
+    result += "?#{url_params.to_param}" if url_params.size > 0
+
+    result
+  end
+
 end
 
 class PDF < Document
