35aa2d37f93a1e40eb76545fbe1b7f41a8783c76
Ben Ehmke <benehmke@gmail.com>
2015-05-28 18:11:45 -0500

    add name function to region

--- lib/mls/region.rb
+++ lib/mls/region.rb
@@ -10,6 +10,14 @@ class Region < MLS::Model
   has_and_belongs_to_many :parents, :join_table => 'regions_regions', :class_name => 'Region', :foreign_key => 'child_id', :association_foreign_key => 'parent_id'
   has_and_belongs_to_many :children, :join_table => 'regions_regions', :class_name => 'Region', :foreign_key => 'parent_id', :association_foreign_key => 'child_id'
   
+  def name
+    if common_name.try(:[], 'eng')
+      common_name['eng'].is_a?(Array) ? common_name['eng'].first : common_name['eng']
+    else
+      official_name['eng'].is_a?(Array) ? official_name['eng'].first : official_name['eng']
+    end
+  end
+  
   def cover_photo_url(options={})
 
     options.reverse_merge!({
