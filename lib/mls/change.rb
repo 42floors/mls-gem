f7071048cfb769b404a022e2ec5064eb2b4c8171
Ben Ehmke <benehmke@gmail.com>
2015-06-04 17:16:10 -0500

    fix getting old models on changes

--- lib/mls/change.rb
+++ lib/mls/change.rb
@@ -5,4 +5,36 @@ class Change < MLS::Model
   has_many :event_actions, :as => :action
   has_many :mistakes
   
+  # Returns the association instance for the given name, instantiating it if it doesn't already exist
+  def association(name) #:nodoc:
+    association = super
+
+    return association if name.to_s != 'subject'
+
+    association.instance_exec do
+      def klass
+        type = owner[reflection.foreign_type]
+        type.presence && type.constantize
+      rescue NameError
+        nil
+      end
+
+      def reader(force_reload=false)
+        if klass
+          if force_reload
+            klass.uncached { reload }
+          elsif !loaded? || stale_target?
+            reload
+          end
+
+          target
+        else
+          nil
+        end
+      end
+    end
+
+    association
+  end
+  
 end
