8152063b5cfdf1cdb26cf7f292a1913b8ca796e3
Ben Ehmke <benehmke@gmail.com>
2015-07-06 14:06:45 -0500

    add photos to coworking space

--- lib/mls/coworking_space.rb
+++ lib/mls/coworking_space.rb
@@ -5,6 +5,7 @@ class CoworkingSpace < MLS::Model
   belongs_to  :organization
   belongs_to  :property
   has_many    :image_orderings, as: :subject, dependent: :destroy
+  has_many    :photos, through: :image_orderings, source: :image
   has_many    :spaces
   has_many    :addresses, :through => :property
 
