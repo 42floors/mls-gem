09d17d17d8640e8bb4aa45e635e2f9930484f811
Ben Ehmke <benehmke@gmail.com>
2016-04-15 15:45:03 -0500

    account_attributes on inquiry

--- lib/mls/inquiry.rb
+++ lib/mls/inquiry.rb
@@ -5,8 +5,35 @@ class Inquiry < MLS::Model
   belongs_to :subject, polymorphic: true
   belongs_to :account
 
+  accepts_nested_attributes_for :account
+
   def property
     subject.is_a? MLS::Model::Listing ? subject.property : subject
+  end
+  
+  def account_attributes=(account_attrs)
+    if account_attrs[:email_addresses_attributes]
+      account = Account.filter(
+        :email_addresses => {
+          :address => account_attrs[:email_addresses_attributes][0][:address].try(:downcase)
+        }
+      ).first
+      if account
+        account_attrs.delete(:email_addresses_attributes)
+        account_attrs[:phones_attributes].each_with_index do |phone_attrs, index|
+          number = PhoneValidator.normalize(phone_attrs[:number])
+          account_attrs[:phones_attributes].delete_at(index) if Phone.where(number: number).count > 0
+        end
+        self.account_id = account.id
+        account_attrs[:id] = account.id
+        account.update_attributes(account_attrs)
+        super
+      else
+        super
+      end
+    else
+      super
+    end
   end
 
 end
--- lib/mls/phone.rb
+++ lib/mls/phone.rb
@@ -4,5 +4,11 @@ class Phone < MLS::Model
   TYPES = ['mobile', 'work', 'home', 'main', 'home fax' 'work fax', 'other fax', 'pager', 'other', 'office']
 
   belongs_to :account
-
+  
+  def number=(value)
+    write_attribute(:number, PhoneValidator.normalize(value))
+    write_attribute(:carrier_name, nil)
+    write_attribute(:carrier_type, nil)
+  end
+  
 end
