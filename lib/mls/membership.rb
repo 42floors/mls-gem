d414e8162d04ba81f7a6b0382e6c763b65e38489
Ben Ehmke <benehmke@gmail.com>
2016-05-05 20:56:44 -0500

    cost attributes on membership

--- lib/mls/membership.rb
+++ lib/mls/membership.rb
@@ -2,4 +2,12 @@ class Membership < MLS::Model
   
   belongs_to :account
   
+  def cost
+    read_attribute(:cost) / 100 if read_attribute(:cost)
+  end
+  
+  def cost_per_property
+    read_attribute(:cost_per_property) / 100 if read_attribute(:cost)
+  end
+  
 end
