a69cc18a881c318a4ca106202d34895c398ce78b
Ben Ehmke <benehmke@gmail.com>
2017-03-14 18:00:48 -0500

    add impression_counts

--- lib/mls/impression_count.rb
+++ lib/mls/impression_count.rb
@@ -1,4 +1,21 @@
 class ImpressionCount < MLS::Model
   belongs_to :subject, polymorphic: true
-
+  
+  def self.by_day(**body)
+    req = Net::HTTP::Get.new("/impression_counts/by_day")
+    req.body = body.to_json
+    connection.instance_variable_get(:@connection).send_request(req).body
+  end
+  
+  def self.by_week(**body)
+    req = Net::HTTP::Get.new("/impression_counts/by_week")
+    req.body = body.to_json
+    connection.instance_variable_get(:@connection).send_request(req).body
+  end
+  
+  def self.by_month(**body)
+    req = Net::HTTP::Get.new("/impression_counts/by_month")
+    req.body = body.to_json
+    connection.instance_variable_get(:@connection).send_request(req).body
+  end
 end
