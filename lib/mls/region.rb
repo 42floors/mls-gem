4de4c4e694c79a11d40827df044a253ac8776d86
Jon Bracy <jonbracy@gmail.com>
2017-09-06 13:17:24 -0700

    fix

--- lib/mls/region.rb
+++ lib/mls/region.rb
@@ -5,7 +5,7 @@ class Region < MLS::Model
   self.inheritance_column = nil
 
   COUNTRY_TYPES = ["Monarchy", "Republic"]
-  STATE_TYPE = ["State", "Territory", "Commonwealth", "Province"]
+  STATE_TYPES = ["State", "Territory", "Commonwealth", "Province"]
   CITY_TYPES = ["City", "Municipality", "Village", "Rural Municipality", "Town", "Resort Village", "Community Government"]
 
   belongs_to :cover_photo, :class_name => 'Image'
