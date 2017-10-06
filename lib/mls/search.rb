96e4f7dc3210740d49500c466f2be2923ce24628
Ben Ehmke <benehmke@gmail.com>
2017-10-06 16:43:49 -0500

    add per_sqft_per_year to searches

--- lib/mls/search.rb
+++ lib/mls/search.rb
@@ -2,7 +2,7 @@ class Search < MLS::Model
 
   STATUS_OPTIONS = %w(active closed nurturing)
   STAGE_OPTIONS = %w(initiated delivered connected toured loi signed)
-  BUDGET_UNITS = %w(per_month per_year)
+  BUDGET_UNITS = %w(per_month per_year per_sqft_per_year)
   TERMS = %w(<1 1-2 3-5 5+ flexible)
   MOVE_INS = %w(<3 3-6 6-12 12+ flexible)
   
