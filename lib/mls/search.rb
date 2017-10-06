ce54f804c5ab187b59d38412103d9d773a2ca4a6
Ben Ehmke <benehmke@gmail.com>
2017-10-06 14:09:55 -0500

    Add helpers to search model for new attributes

--- lib/mls/search.rb
+++ lib/mls/search.rb
@@ -2,6 +2,9 @@ class Search < MLS::Model
 
   STATUS_OPTIONS = %w(active closed nurturing)
   STAGE_OPTIONS = %w(initiated delivered connected toured loi signed)
+  BUDGET_UNITS = %w(per_month per_year)
+  TERMS = %w(<1 1-2 3-5 5+ flexible)
+  MOVE_INS = %w(<3 3-6 6-12 12+ flexible)
   
   belongs_to :account
   belongs_to :broker, class_name: "Account"
@@ -19,5 +22,29 @@ class Search < MLS::Model
   def regions
     Region.where(id: self.region_ids)
   end
-
+  
+  def move_in_units(value=nil)
+    value ||= self.move_in
+    case value
+    when "<1"
+      "month"
+    when "flexible"
+      ""
+    else
+      "months"
+    end
+  end
+  
+  def term_units(value=nil)
+    value ||= self.term
+    case value
+    when "<1"
+      "year"
+    when "flexible"
+      ""
+    else
+      "years"
+    end
+  end
+  
 end
