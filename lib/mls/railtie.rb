b7b28732e86fadd8212f8abbd9adaecedf08c72f
James Bracy <waratuman@gmail.com>
2018-10-15 11:55:10 -0400

    Update Railtie to support old way of loading MLS as well with notice

--- lib/mls/railtie.rb
+++ lib/mls/railtie.rb
@@ -21,6 +21,15 @@ class MLS::Railtie < Rails::Railtie
         h[:adapter] = 'sunstone'
       end
       h
+    elsif app.config.mls.fetch('url').is_a?(String)
+      ActiveSupport::Deprecation.warn("Using mls.yml has been deprecated. Move to secrets.yml")
+      h = ActiveRecord::ConnectionAdapters::ConnectionSpecification::ConnectionUrlResolver.new(app.config.mls.fetch('url')).to_hash.symbolize_keys
+      h[:api_key] = h.delete(:username)
+      if h[:adapter] != 'sunstone'
+        h[:use_ssl] = h[:adapter] == 'https'
+        h[:adapter] = 'sunstone'
+      end
+      h
     else
       app.secrets.mls
     end
@@ -31,10 +40,10 @@ class MLS::Railtie < Rails::Railtie
       app.class.name.split('::')[0..-2].join('::')
     }
     user_agent << "Rails/#{Rails.version}"
-    h[:user_agent] = user_agent.compact.join(' ')
+    configs[:user_agent] = user_agent.compact.join(' ')
     
     
-    MLS::Model.establish_connection(h)
+    MLS::Model.establish_connection(configs)
   end
   
 end
